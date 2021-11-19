4b21efaf300a57005c8bcf83ca032328d5f4a3aa
Jill Naiman <jnaiman@gmail.com>
2021-11-19 11:29:42 -0600

    maybe fixed

--- post_processing_utils.py
+++ post_processing_utils.py
@@ -1627,6 +1627,10 @@ def expand_found_boxes_fig_cap(boxes_sq, labels_sq, scores_sq, bbsq,
             fig_cap_pair.append([ibb,iout])
         else: # if no, mark as no caption
             fig_cap_pair.append([ibb,-1]) # nothing found
+            
+    #print(fig_cap_pair)
+    #print('boxes_fig',boxes_fig)
+    #print('boxes_cap',boxes_cap)
 
     # expand fig->caption for overlaps
     # NOTE: this associates each figure with a caption -- i.e., no "floating" captions
@@ -1644,13 +1648,20 @@ def expand_found_boxes_fig_cap(boxes_sq, labels_sq, scores_sq, bbsq,
             xShift = (xcc-xcf)/config.IMAGE_W; 
             yShift = -1.0*(ycc-ycf)/config.IMAGE_H
             theta = np.arctan2(yShift,xShift)*180/np.pi
+            #print(theta)
             # arctan2 in range [-180,180]
             if (theta < -70) and (theta > -110): # caption on bottom
-                bf[0] = min(bc[0],bf[0]); bf[2] = max(bf[2],bc[2])
+                bf[3] = bc[1]
             elif (theta < 20) and (theta > -20): # caption on right
-                bf[1]=min(bc[1],bf[1]); bf[3]=max(bf[3],bc[3])
+                bf[2] = bc[0]
             elif (theta > 160) or (theta < -160):
-                bf[1]=min(bc[1],bf[1]); bf[3]=max(bf[3],bc[3])
+                bf[0] = bc[2] 
+            # if (theta < -70) and (theta > -110): # caption on bottom
+            #     bf[0] = min(bc[0],bf[0]); bf[2] = max(bf[2],bc[2])
+            # elif (theta < 20) and (theta > -20): # caption on right
+            #     bf[1]=min(bc[1],bf[1]); bf[3]=max(bf[3],bc[3])
+            # elif (theta > 160) or (theta < -160):
+            #     bf[1]=min(bc[1],bf[1]); bf[3]=max(bf[3],bc[3])
         boxesOut.append(bf) # either way, add it on
         labelsOut.append(labels_fig[f[0]])
         scoresOut.append(scores_fig[f[0]])
@@ -1658,6 +1669,8 @@ def expand_found_boxes_fig_cap(boxes_sq, labels_sq, scores_sq, bbsq,
     for b,l,s in zip(boxes_others, labels_other, scores_other):
         boxesOut.append(b); labelsOut.append(l); 
         scoresOut.append(s)
+        
+    #print(boxesOut,labelsOut,scoresOut)
 
     # yet again we have to do this... can we do this just once here?
     for ibox in range(len(boxesOut)):
@@ -1681,151 +1694,3 @@ def expand_found_boxes_fig_cap(boxes_sq, labels_sq, scores_sq, bbsq,
 
 
 
-# # FP/FN/TP calcs:
-# def calc_metrics(truebox1, boxes_sq, labels_sq, scores_sq, LABELS,ioumin,
-#                 years=[], iioumin=-1, iscoremin=-1, 
-#                 TPyear = [], FPyear=[], totalTrueyear=[], FNyear=[], year=[],
-#                 totalTruev=[], TPv=[], FPv=[],FNv=[], return_pairs = False):
-#     """
-#     truebox1: trueboxes in YOLO coords (typically 512x512) (xmin,ymin,xmax,ymax, LABEL_INDEX+1)
-#     boxes_sq: found boxes in YOLO coords (xmin,ymin,xmax,ymax)
-#     labels_sq: found box labels (LABEL_INDEX) -- NOTE no +1!
-#     scores_sq: found box score (0.0-1.0)
-#     LABELS: list of labels (strings), e.g. ['figure', 'figure caption', 'table']
-#     """
-#     # checks
-#     if iioumin == -1 and iscoremin != -1: 
-#         print('not supported for different params for iioumin & iscoremin')
-#         import sys; sys.exit()
-#     # other checks
-#     if iioumin == -1 and iscoremin == -1:
-#         totalTruev = np.zeros(len(LABELS))
-#         FNv = np.zeros(len(LABELS))
-#         FPv = np.zeros(len(LABELS))
-#         TPv = np.zeros(len(LABELS))
-#         if len(years)>0:
-#             totalTrueyear = np.zeros([len(years),len(LABELS)])
-#             FNyear = np.zeros([len(years),len(LABELS)])
-#             FPyear = np.zeros([len(years),len(LABELS)])
-#             TPyear = np.zeros([len(years),len(LABELS)])
-
-#     # make array of total number of boxes true/found, tag extra found with a FP tag
-#     # loop and find closest boxes
-#     true_found_index = []; true_found_labels = []; trueCaps = []; foundCaps = []; #iouSave = []
-#     for it,tbox in enumerate(truebox1): # if there really is a box
-#         w2, h2 = tbox[2]-tbox[0], tbox[3]-tbox[1]
-#         x2, y2 = tbox[0]+0.5*w2, tbox[1]+0.5*h2
-#         # just for found captions
-#         if iioumin != -1:
-#             totalTruev[int(tbox[4]-1),iioumin,iscoremin] += 1
-#             if len(years)>0:
-#                 totalTrueyear[years==year,int(tbox[4]-1),iioumin,iscoremin] += 1                
-#         else:
-#             totalTruev[int(tbox[4]-1)] += 1
-#             if len(years) >0:
-#                 totalTrueyear[years==year,int(tbox[4]-1)] += 1                
-            
-#         trueCaps.append(tbox)
-#         # find greatest IOU -- literally there is a better algorithm here to do this
-#         iouMax = -10
-#         foundBox = False
-#         indFound = [it,-1]; labelsFound = [tbox[-1]-1, -1]; foundCapHere = []
-#         for ib,b in enumerate(boxes_sq):
-#             isOverlapping = isRectangleOverlap(tbox[:-1],b)
-#             w1, h1 = b[2]-b[0], b[3]-b[1]
-#             x1, y1 = b[0]+0.5*w1, b[1]+0.5*h1
-#             iou1 = iou_orig(x1,y1,w1,h1, x2,y2,w2,h2)
-#             # a win!
-#             if (iou1 > iouMax) and (iou1 > ioumin) and isOverlapping: # check for overlap
-#                 iouMax = iou1
-#                 indFound[-1] = ib
-#                 labelsFound[-1] = labels_sq[ib]
-#                 foundCapHere = b
-#                 #print('yes')
-#         true_found_index.append(indFound); true_found_labels.append(labelsFound); 
-#         if len(foundCapHere) > 0: foundCaps.append(foundCapHere)
-        
-#     # count
-#     # save pairs, unfound trues, miss-found founds
-#     true_found_pairs = []
-#     for ti,tl in zip(true_found_index, true_found_labels): # ti = [true index, found index]
-#         ind = int(tl[0]) # index is true's label
-#         if ti[-1] == -1: # didn't find anything
-#             if iioumin != -1:
-#                 FNv[ind,iioumin,iscoremin] +=1
-#                 if len(years)>0:
-#                     FNyear[years==year,ind,iioumin,iscoremin] += 1
-#             else:
-#                 FNv[ind] += 1
-#                 if len(years)>0:
-#                     FNyear[years==year,ind] +=1
-#             # save as a true w/o a found
-#             true_found_pairs.append( (truebox1[ti[0]], -1) )
-#         elif ti[-1] != -1 and tl[0] != tl[1]: # overlap of boxes, but wrong things -- count as FN for this true
-#             if iioumin != -1:
-#                 FNv[ind,iioumin,iscoremin] +=1
-#                 if len(years)>0:
-#                     FNyear[years==year,ind,iioumin,iscoremin] += 1
-#             else:
-#                 FNv[ind] += 1
-#                 if len(years)>0:
-#                     FNyear[years==year,ind] +=1
-#             # save as a true w/o a found
-#             true_found_pairs.append( (truebox1[ti[0]], -1) )
-#         elif ti[-1] != -1 and tl[0] == tl[1]: # found a box AND its the right one!
-#             if iioumin != -1:
-#                 TPv[ind,iioumin,iscoremin] +=1
-#                 if len(years)>0:
-#                     TPyear[years==year,ind,iioumin,iscoremin] += 1
-#             else:
-#                 TPv[ind] += 1
-#                 if len(years)>0:
-#                     TPyear[years==year,ind] +=1
-#             #try:
-#             true_found_pairs.append( (truebox1[ti[0]], (boxes_sq[ti[1]][0],boxes_sq[ti[1]][1],
-#                                                     boxes_sq[ti[1]][2],boxes_sq[ti[1]][3],labels_sq[ti[1]])) )
-            
-#     # do we have extra found boxes?
-#     if len(boxes_sq) > len(trueCaps):
-#         for ib, b in enumerate(boxes_sq):
-#             if len(true_found_index) > 0: # we have some trues
-#                 if ib not in np.array(true_found_index)[:,1].tolist(): # but we don't have this particular found matched to a true
-#                     ind = labels_sq[ib] # label will be found label -- mark as a FP for this label
-#                     if iioumin != -1:
-#                         FPv[ind,iioumin,iscoremin] +=1
-#                         if len(years)>0:
-#                             FPyear[years==year,ind,iioumin,iscoremin] += 1
-#                     else:
-#                         FPv[ind] += 1
-#                         if len(years)>0:
-#                             FPyear[years==year,ind] +=1
-#                     # mark as a found w/o a true
-#                     true_found_pairs.append( (-1, (boxes_sq[ib][0],boxes_sq[ib][1],
-#                                                     boxes_sq[ib][2],boxes_sq[ib][3],labels_sq[ib])) )
-#             elif len(true_found_index) == 0: # there is nothing true, any founds are FP
-#                 ind = labels_sq[ib]
-#                 if iioumin != -1:
-#                     FPv[ind,iioumin,iscoremin] +=1
-#                     if len(years)>0:
-#                         FPyear[years==year,ind,iioumin,iscoremin] += 1
-#                 else:
-#                     FPv[ind] += 1
-#                     if len(years)>0:
-#                         FPyear[years==year,ind] +=1
-#                 # mark as a found w/o a true
-#                 true_found_pairs.append( (-1, (boxes_sq[ib][0],boxes_sq[ib][1],
-#                                                 boxes_sq[ib][2],boxes_sq[ib][3],labels_sq[ib])) )
-
-               
-#     if len(years)>0:
-#         if not return_pairs:
-#             return totalTruev, TPv, FPv, FNv, totalTrueyear, TPyear, FPyear, FNyear
-#         else:
-#             return totalTruev, TPv, FPv, FNv, totalTrueyear, TPyear, FPyear, FNyear, true_found_pairs
-#     else:
-#         if not return_pairs:
-#             return totalTruev, TPv, FPv, FNv
-#         else:
-#             return totalTruev, TPv, FPv, FNv,true_found_pairs
-
-    
